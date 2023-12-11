0494199b3fa9cb833a22ba8c418f844a13667a93
Jobie Kirkwood <jobie.kirkwood@york.ac.uk>
2023-12-11 01:47:34 +0000

    - change: api controller

--- OptixMovieAPI/API/Controllers/MovieController.cs
+++ OptixMovieAPI/API/Controllers/MovieController.cs
@@ -10,26 +10,22 @@ namespace API.Controllers
     {
 
         private readonly ILogger<MovieController> _logger;
-        private readonly AppDbContext _appDbContext;
+       
 
-        public MovieController(ILogger<MovieController> logger, AppDbContext appDbContext)
+        public MovieController(ILogger<MovieController> logger)
         {
             _logger = logger;
-            _appDbContext = appDbContext;
+            
         }
 
         [HttpGet(Name = "GetMovieByTitle")]
         public ActionResult<IEnumerable<Movie>> Get(string title)
         {
-            if (string.IsNullOrEmpty(title))
-            {
-                return NoContent();
-            }
-            else
-            {
+         
+           
                 return new List<Movie>() { new Movie { Title = "onemovie" }, new Movie { Title = "twoMovie" } };
-                return _appDbContext.Movies.Where(x => x.Title.Contains(title)).ToList();
-            }
+                //return _appDbContext.Movies.Where(x => x.Title.Contains(title)).ToList();
+           
 
 
 
